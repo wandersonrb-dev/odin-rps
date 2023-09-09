f5e8129355aed24d5a7fb6d535480fcd374a9b4d
Wanderson_RB <wandersonrb.dev@gmail.com>
2023-09-09 20:09:56 -0300

    Define function to get the computer choice

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+
+function getComputerChoice() {
+    switch (random(3)) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper'
+        default:
+            return 'scissors'
+    }
+}
+
 // Return a random number between 0 up to no include upperBound 
 function random(upperBound = 3) {
     return Math.floor(Math.random() * upperBound);
